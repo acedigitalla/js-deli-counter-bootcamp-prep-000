3d3dc63d79ee72b18bb82c15b81aae7b43c92724
Alex Edwards <info@acedigitalagency.net>
2018-03-28 04:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
-  return "Currently serving " + katzDeliLine.shift() + ".";
+  return "Currently serving " + katzDeliLine.shift(0) + ".";
 }
 
 function currentLine(katzDeliLine) {
