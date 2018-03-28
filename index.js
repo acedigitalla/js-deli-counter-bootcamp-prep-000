18e331b34aa7036df3892d330e9a6cfe3fdacf17
Alex Edwards <info@acedigitalagency.net>
2018-03-28 04:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing() {
-  katzDeliLine.shift;
-  return "There is nobody waiting to be served!";
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  return "Currently serving" + katzDeliLine.shift() + ".";
 }
 
 function currentLine(katzDeliLine) {
